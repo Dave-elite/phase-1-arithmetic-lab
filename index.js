434b5377b23df6c2abab499961b1c769cff3fe84
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-06-27 22:20:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+let random = 0;
+random++
+
+let num3 = 5;
+let num4 = 20;
+let mod = num4 / num3
+
+let max = 20
+Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
